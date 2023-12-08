8afb1406101b55bccad448a8fc947cae828a5944
elitex45 <gurugkgurukiran@gmail.com>
2023-12-09 00:17:57 +0530

    added the base rental contract

---
+++ contracts/interfaces/IRental.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+interface IRental {
+    // owner functions
+    struct AddListingStruct {
+        uint256 propertyId;
+        address owner;
+        uint256 initialDeposit;
+        uint256 rent;
+        string location;
+    }
+
+    function addListing(
+        uint256 propertyId,
+        address owner,
+        uint256 initialDeposit,
+        uint256 rent,
+        string memory location
+    ) external;
+
+    function updateListingStatus(uint256 propertyId) external;
+
+    function claimSecurityDeposit(uint256 propertyId) external;
+
+    function getListingByOwnerAddress(address _owner) external view;
+
+    function getRentStatus(uint256 propertyId) external view;
+
+    function getRentStatusByOwnerAddress(address _owner) external view;
+
+    // user functions
+    function paySecurityDeposit(uint256 propertyId) external;
+
+    function payRent(uint256 propertyId) external;
+
+    // common functions
+    function confirmOccupation(uint256 propertyId, bool isConfirmed) external;
+
+    function getAllPropertyListings() external view;
+}
---
+++ contracts/rentalCore/Storage.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Storage {
+    struct Property {
+        uint256 propertyId;
+        address owner;
+        uint256 securityDeposit;
+        uint256 rent;
+        uint256 waitingPeriod; // to claim the deposit and list it again
+        string propertyLocation;
+        bool isReserved;
+        bool isConfirmedByTenent;
+        bool isConfirmedByOwner;
+        bool rentPaid;
+        bool propertyListingStatus;
+        bool securityDepositClaimedStatus;
+    }
+    uint256 totalProperties;
+    mapping(uint256 => Property) propertyIdToProperty;
+    mapping(address => uint256[]) ownerToPropertyIds;
+}
