25da2d8bdafe73d82ced888e4b477aaa5d39c118
HAPPYS1NGH <harpreetsingh140503@gmail.com>
2023-12-09 01:13:35 +0530

    initial setup complete

---
+++ application/constants/index.js
@@ -0,0 +1,74 @@
+export const footerTextTop = [
+  {
+    id: "discourse",
+    title: "Discourse",
+    content: "Discuss governance proposals",
+  },
+  {
+    id: "medium",
+    title: "Medium",
+    content: "Long-form updates and news",
+  },
+  {
+    id: "legal",
+    title: "Legal ",
+    content: "Disclaimer & terms of service",
+  },
+  {
+    id: "documentation",
+    title: "Documentation ",
+    content: "Learn more about Radiant",
+  },
+  {
+    id: "security",
+    title: "Security ",
+    content: "Bug bounties",
+  },
+];
+
+export const footerTextDown = [
+  {
+    id: "snapshot",
+    title: "Snapshot",
+    content: "Vote on live proposals",
+  },
+  {
+    id: "mirror",
+    title: "Mirror",
+    content: "Radiant's monthly newsletter",
+  },
+  {
+    id: "career",
+    title: "Career",
+    content: "Join our team",
+  },
+  {
+    id: "support_center",
+    title: "Support Center",
+    content: "Submit feedback and report issues",
+  },
+  {
+    id: "status",
+    title: "Status",
+    content: "Website uptime",
+  },
+];
+
+export const socialsText = [
+  {
+    id: "discord",
+    title: "Discord",
+  },
+  {
+    id: "twitter",
+    title: "Twitter",
+  },
+  {
+    id: "telegram",
+    title: "Telegram",
+  },
+  {
+    id: "youtube",
+    title: "YouTube",
+  },
+];
