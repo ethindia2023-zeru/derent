28e1bf46ac52c42409f017c098a7d315073f8f40
HAPPYS1NGH <harpreetsingh140503@gmail.com>
2023-12-09 01:12:11 +0530

    feat: added initial page

---
+++ application/components/shared/footer/Footer.jsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+import { footerTextDown, footerTextTop, socialsText } from "@/constants";
+// import { styles } from "@/styles/styles_market";
+// import image1 from "/public/big_logo.svg";
+
+export const Footer = () => {
+    return (
+        <section className="flex h-[354px] w-full items-center justify-center bg-[#F5F6FA]  pt-[50px]">
+            footer
+        </section>
+    );
+};
