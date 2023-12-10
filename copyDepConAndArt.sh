e06a2b54f99baafe98414782fbf67399cf2722b5
elitex45 <gurugkgurukiran@gmail.com>
2023-12-10 11:34:47 +0530

    a

--- copyDepConAndArt.sh
+++ copyDepConAndArt.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Run script1.sh with arguments
-./sdc.sh /Users/akshaysp/Work/ethindia_hackathon/derent/deployedAddresses.json /Users/akshaysp/Work/ethindia_hackathon/derent/application/contractConfig/deployedAddresses.json
+./sdc.sh /Users/engineer/Documents/workspace/EthIndia23/eth/derent/deployedAddresses.json /Users/engineer/Documents/workspace/EthIndia23/eth/derent/application/contractConfig/deployedAddresses.json
 
 echo "SDC Done"
 
-./sart.sh /Users/akshaysp/Work/ethindia_hackathon/derent/artifacts /Users/akshaysp/Work/ethindia_hackathon/derent/application/artifacts
+./sart.sh /Users/engineer/Documents/workspace/EthIndia23/eth/derent/artifacts /Users/engineer/Documents/workspace/EthIndia23/eth/derent/application/artifacts
 # Continue with your current script
 echo "SART Done"
